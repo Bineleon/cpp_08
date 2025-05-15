9be98c5f127f525792fe33cc71117d5642e8ac40
Nelbi Leon <nelbi.leon@gmail.com>
2025-05-15 14:04:31 +0200

    cpp08: ONGOING

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -19,9 +19,10 @@ void printHeader(const std::string& str)
 int main()
 {
     {
+        printHeader("Test: Basic");
         Span sp = Span(5);
 
-        sp.addNumber(-6);
+        sp.addNumber(6);
         sp.addNumber(3);
         sp.addNumber(17);
         sp.addNumber(9);
@@ -31,16 +32,32 @@ int main()
         std::cout << sp.longestSpan() << std::endl;
     }
     {
-        Span sp = Span(15000);
+        printHeader("Test: Big range");
+        Span sp = Span(20000);
         std::list<int> v;
         std::srand(time(NULL));
 
         try
         {
             for (int i = 0; i < 20000; ++i)
-            {
-                v.push_back(std::rand() % 10000);
-            }
+                v.push_back(std::rand() % 100000);
+            sp.addRange(v.begin(), v.end());
+            std::cout << "Shortest span : " << SMGREEN << sp.shortestSpan() << RESET << std::endl;
+            std::cout << "Longest span : " << SMGREEN << sp.longestSpan() << std::endl;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << SMRED << e.what() << RESET << '\n';
+        }
+    }
+        {
+        printHeader("Test: Big range with negative value");
+        Span sp = Span(50000);
+        std::list<int> v;
+        try
+        {
+            for (int i = -10000; i < 20000; ++i)
+                v.push_back(i);
             sp.addRange(v.begin(), v.end());
             std::cout << sp.shortestSpan() << std::endl;
             std::cout << sp.longestSpan() << std::endl;
@@ -49,18 +66,24 @@ int main()
         {
             std::cerr << SMRED << e.what() << RESET << '\n';
         }
-
-        // std::vector<int> v2 = sp.getV();
-        // for (int i = 0; i < sp.size(); ++i)
-        //     std::cout << v2[i] << std::endl;
     }
-
-    // sp.addNumber(6);
-    // sp.addNumber(3);
-    // sp.addNumber(17);
-    // sp.addNumber(9);
-    // sp.addNumber(11);
-    // std::cout << sp.shortestSpan() << std::endl;
-    // std::cout << sp.longestSpan() << std::endl;
+    {
+        printHeader("Test: Container full");
+        Span sp = Span(15000);
+        std::vector<int> v;
+        std::srand(time(NULL));
+        try
+        {
+            for (int i = 0; i < 20000; ++i)
+                v.push_back(std::rand() % 20000);
+            sp.addRange(v.begin(), v.end());
+            std::cout << sp.shortestSpan() << std::endl;
+            std::cout << sp.longestSpan() << std::endl;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << SMRED << e.what() << RESET << '\n';
+        }
+    }
     return 0;
 }
